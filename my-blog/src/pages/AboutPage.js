3796cb806e64572f605b73bc18d0515d324a5f7c
Asthio <57274664+Asthio@users.noreply.github.com>
2020-06-03 14:32:48 +0200

    2nd commit

---
+++ my-blog/src/pages/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+        <h1>About me</h1>
+    </>
+    
+)
+
+export default AboutPage;
---
+++ my-blog/src/pages/article-content.js
@@ -0,0 +1,27 @@
+const articles = [
+    {
+        name: 'learn-react',
+        title: 'The fastest way to learn React',
+        content: [
+            `Welcome! Today we're going to be talking about the fastest way to learn React.`
+        ]
+    },
+
+    {
+        name: 'what-i-do',
+        title: 'Look what Im doing every day',
+        content: [
+            `Welcome! Today we're going to be talking about my every day`
+        ]
+    },
+
+    {
+        name: 'my-hobbys',
+        title: 'Look what I like',
+        content: [
+            `Welcome! Today we're going to be talking about my hobbys`
+        ]
+    },
+];
+
+export default articles;
