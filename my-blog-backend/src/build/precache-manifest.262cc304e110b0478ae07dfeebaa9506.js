c2ae4f877d79526757f2f24c28efe549058ab7f5
Asthio <57274664+Asthio@users.noreply.github.com>
2020-06-03 14:40:25 +0200

    3rd commit

---
+++ my-blog-backend/src/build/precache-manifest.262cc304e110b0478ae07dfeebaa9506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e999b88153484c70122a41bd94e3a829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb630aa82299bd6ebf46",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "892941cff0311a6b4cfe",
+    "url": "/static/js/2.51fe4752.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.51fe4752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb630aa82299bd6ebf46",
+    "url": "/static/js/main.8b7172d8.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
