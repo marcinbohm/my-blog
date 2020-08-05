8da0987118ee9265c6e1e25c6c6b2e85d83d35c9
Asthio <57274664+Asthio@users.noreply.github.com>
2020-08-05 19:41:31 +0200

    styled-components installed, homepage content style edited

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,20 +1,50 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
 
-const NavBar = () => (
-    <nav>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-            <li>
-                <Link to="/articles-list">Articles</Link>
-            </li>
-        </ul>
-    </nav>
+const Navbar = styled.header`
+        position: fixed;
+        box-sizing: border-box;
+        width: 100%;
+        max-width: 100%;
+        padding: 5px 10px;
+        margin: 0px auto;
+        justify-content: space-between;
+        display: flex;
+        text-decoration: none;
+        background-color: gray;
+    `;
+
+const Menu = styled.ul`
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        overflow: hidden;
+        li {
+            display: block;
+            padding: 10px;
+            text-decoration: none;
+            font-size: 28px;
+            float: left;
+        }
+    `;
+
+const Header = () => (
+    <Navbar>
+        <Menu>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/articles-list">Articles</Link>
+                </li>
+            </ul>
+        </Menu>
+    </Navbar>
 );
 
-export default NavBar;
+export default Header;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,31 +4,50 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    title: {
-      backgroundColor: "#384663",
-      height: "50px",
-      padding: theme.spacing(6, 0),
+    home: {
+        height: "100%",
     },
+    title: {
+        backgroundColor: "#384663",
+        height: "50px",
+        padding: theme.spacing(6, 0),
+    },
+    main: {
+        backgroundColor: "#124663",
+        height: "800px",
+        padding: theme.spacing(6, 0),
+    },
+
   }));
 
-function Title(props) {
+function HomePageContent(props) {
+
     const classes = useStyles();
     const {title} = props;
 
     return(
-        <div className={classes.title}>
-            <Container maxWidth= "lg">
-                <Typography variant="h3" align="center" gutterBottom>
-                    {title}
-                </Typography>
-            </Container>
+        <div className={classes.home}>
+            <div className={classes.title}>
+                <Container maxWidth= "lg">
+                    <Typography variant="h3" align="center" gutterBottom>
+                        {title}
+                    </Typography>
+                </Container>
+            </div>
+            <div className={classes.main}>
+                <Container maxWidth= "lg">
+                    <Typography align="center">
+                        Home page content
+                    </Typography>
+                </Container>
+            </div>
         </div>
     )
 }
 
 const HomePage = () => {
     return(
-        <Title title="Blog about my journey" />
+        <HomePageContent title="Blog about my journey" />
     )
 }
 
